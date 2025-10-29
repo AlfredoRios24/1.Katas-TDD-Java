757ab32435fc760d93956ba6bc6da3df4469740b
AlfredoRios24 <alfredoerios_03@hotmail.com>
2025-10-29 10:28:50 +0100

    first testing in FizzBuzz JavaScript

--- 01-fizzbuzz/fizzbuzz-js/fizzbuzz.js
+++ 01-fizzbuzz/fizzbuzz-js/fizzbuzz.js
@@ -1,7 +1,20 @@
+function fizzbuzz(number) {
+  if (typeof number !== "number") throw new Error("parameter must be a number");
 
-export const fizzbuzz = (number) => {
-    if (typeof number !== 'number') throw new Error('parameter provided must be a number');
-    if (Number.isNaN(number)) throw new Error('parameter provided must be a number');
+  if (number % 15 === 0) return "FizzBuzz";
+  if (number % 3 === 0) return "Fizz";
+  if (number % 5 === 0) return "Buzz";
 
-    return number;
+  return number;
 }
+
+module.exports = { fizzbuzz };
+
+// Permite ejecutarlo como script
+if (require.main === module) {
+  for (let i = 1; i <= 100; i++) {
+    console.log(fizzbuzz(i));
+  }
+}
+
+//npm start
--- 01-fizzbuzz/fizzbuzz-js/fizzbuzz.test.js
+++ 01-fizzbuzz/fizzbuzz-js/fizzbuzz.test.js
@@ -2,7 +2,36 @@ import { describe, it, expect } from "vitest";
 import { fizzbuzz } from "./fizzbuzz.js";
 
 describe('fizzbuzz', () => {
-    it('should be a function', () => {
-        expect(typeof fizzbuzz).toBe('function');
-    });
+
+  it('should be a function', () => {
+    expect(typeof fizzbuzz).toBe('function');
+  });
+
+  it('should throw if parameter is not a number', () => {
+    expect(() => fizzbuzz()).toThrow();
+    expect(() => fizzbuzz("1")).toThrow();
+    expect(() => fizzbuzz(null)).toThrow();
+  });
+
+  it('should return Fizz if number is divisible by 3', () => {
+    expect(fizzbuzz(3)).toBe("Fizz");
+    expect(fizzbuzz(6)).toBe("Fizz");
+  });
+
+  it('should return Buzz if number is divisible by 5', () => {
+    expect(fizzbuzz(5)).toBe("Buzz");
+    expect(fizzbuzz(10)).toBe("Buzz");
+  });
+
+  it('should return FizzBuzz if number is divisible by 3 and 5', () => {
+    expect(fizzbuzz(15)).toBe("FizzBuzz");
+    expect(fizzbuzz(30)).toBe("FizzBuzz");
+  });
+
+  it('should return the number if no rule is matched', () => {
+    expect(fizzbuzz(7)).toBe(7);
+  });
+
 });
+
+//npm test
